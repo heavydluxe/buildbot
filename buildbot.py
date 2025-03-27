cc9898beffd43f619eba028202aab14554acbfb0
Brian Dellinger <bdellinger@gmail.com>
2025-03-27 16:02:11 -0400

    Updates including looping for brew and the like

--- buildbot.py
+++ buildbot.py
@@ -1,100 +1,102 @@
 import os, sys, time
     
 def restore_brews():
+    # Brew Items - CLIs and Casks
+    brew_clis = ['bat', 'colima', 'coreutils', 'docker', 'docker-completion',
+                 'dockutil', 'emacs', 'figlet', 'gh', 'macmon', 'nmap',
+                 'oh-my-posh', 'ollama', 'speedtest-cli', 'sqlite',
+                 'tcpdump', 'termshark', 'tree']
+    
+    brew_casks = ['1password', 'font-jetbrains-mono',
+                  'font-jetbrains-mono-nerd-font',
+                  'font-meslo-lg-nerd-font', 'obs', 'splashtop-business',
+                  'spotify', 'visual-studio-code', 'windows-app']
+    
+    # Run th restoration loops.    
     print("Restoring Homebrew Files")
-    # Start with brew cli-ish tools
     print("Installing Homebrew CLI files")
-    brew_clis = ['emacs', 'coreutils', 'gh', 'oh-my-posh', 'sqlite',
-                 'ollama', 'colima', 'docker', 'docker-completion',
-                 'macmon', 'nmap', 'speedtest-cli', 'bat', 'tcpdump',
-                 'termshark', 'figlet', 'dockutil', 'tree']
     for brew in brew_clis:
         print(f"Installing {brew} via (home)brew")
-        os.sys(f'brew install {brew}')
+        os.system(f'brew install {brew}')
         time.sleep(1)
-    
-    # Now to brew casks installation
     print("Installing Homebrew Casks")
-    brew_casks = ['font-jetbrains-mono', 'font-jetbrains-mono-nerd-font',
-                  'font-meslo-lg-nerd-font','1password', 'spotify', 'obs', 
-                  'visual-studio-code', 'splashtop-business', 'windows-app']
     for cask in brew_casks:
         print(f"Installing {cask} via (home)brew")
-        os.sys(f'brew install {cask}')
+        os.system(f'brew install {cask}')
         time.sleep(1)
 
 def restore_settings():
     # Set Sysname
     hostname = input("Enter NEW Hostname for this device: ")
     print("You will need to enter your sudo password to proceed")
-    os.sys(f'sudo scutil --set HostName "{hostname.lower()}"')
-    os.sys(f'sudo scutil --set ComputerName "{hostname.lower()}"')
-    os.sys(f'sudo scutil --set LocalHostName "{hostname.lower()}"')
+    os.system(f'sudo scutil --set HostName "{hostname.lower()}"')
+    os.system(f'sudo scutil --set ComputerName "{hostname.lower()}"')
+    os.system(f'sudo scutil --set LocalHostName "{hostname.lower()}"')
     print(f'This computer has been renamed {hostname.lower()}')
     
     # Configure git
     print("Configuring git...")
-    os.sys('git config --global user.name "Brian Dellinger"')
-    os.sys('git config --global user.email "bdellinger@gmail.com"')
-    os.sys('git config --global init.defaultBranch main')
+    os.system('git config --global user.name "Brian Dellinger"')
+    os.system('git config --global user.email "bdellinger@gmail.com"')
+    os.system('git config --global init.defaultBranch main')
     
     # OhMyZsh & related secret prep
-    os.sys('sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"')
-    os.sys('mkdir ~/.cache')
-    os.sys('git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting')
-    os.sys('git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions')
+    os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"')
+    os.system('mkdir ~/.cache')
+    os.system('git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting')
+    os.system('git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions')
     
     # System Files Restore
     print('Restoring System Files from Repo')
-    os.sys('cp ~/sbemode/buildbot/configs/backup.emacs.lsp ~/.emacs')
-    os.sys('cp ~/sbemode/buildbot/configs/backup.zshrc ~/.zshrc')
-    os.sys('cp ~/sbemode/buildbot/configs/backup.mytheme.omp.json ~/.mytheme.omp.json')
+    os.system('cp ~/sbemode/buildbot/configs/backup.emacs.lsp ~/.emacs')
+    os.system('cp ~/sbemode/buildbot/configs/backup.zshrc ~/.zshrc')
+    os.system('cp ~/sbemode/buildbot/configs/backup.mytheme.omp.json ~/.mytheme.omp.json')
     
     # Dock Cleanup
+    dock_apps = ['/Applications/Visual Studio Code.app',  
+                 '/Applications/Firefox.app',
+                 'Applications/Google Chrome.app',
+                 '/System/Applications/System Settings.app',
+                 '/Applications/1Password.app',
+                 '/Applications/Windows App.app',
+                 '/Applications/zoom.us.app',
+                 '/Applications/GlobalProtect.app',
+                 '/Applications/Splashtop Business.app',
+                 '/Applications/Spotify.app']
     print('Installing dockutil and clearing the decks... er, docks.')
-    os.sys('dockutil --remove all')
-    echo "Setting up the Dock"
-    os.sys('dockutil --add /Applications/Google\ Chrome.app')
-    os.sys('dockutil --add /System/Applications/Utilities/Terminal.app')
-    os.sys('dockutil --add /Applications/Firefox.app')
-    os.sys('dockutil --add /Applications/Visual\ Studio\ Code.app')
-    os.sys('dockutil --add /System/Applications/System\ Settings.app')
-    os.sys('dockutil --add /Applications/1Password.app')
-    os.sys('dockutil --add /Applications/Windows\ App.app')
-    os.sys('dockutil --add /Applications/zoom.us.app')
-    os.sys('dockutil --add /System/Applications/TextEdit.app')
-    os.sys('dockutil --add /Applications/Spotify.app')
-    os.sys('dockutil --add /Applications/Splashtop\ Business.app')
-    os.sys('dockutil --add /Applications/GlobalProtect.app')
-    os.sys("dockutil --add /dockutil --add '~/Downloads' --view fan --display folder")
+    os.system('dockutil --remove all')
+    print("Setting up the Dock")
+    for apps in dock_apps:
+        os.system(f'dockutil --add {apps}')
+    os.system("dockutil --add /dockutil --add '~/Downloads' --view fan --display folder")
 
 def launch_apps():
     # Start the GUI apps and get them configured...
     print('Opening key apps for configuration.')
-    os.sys('open -n /Applications/Google\ Chrome.app')
-    os.sys('open -n /Applications/1Password.app')
-    os.sys('open -n /Applications/Firefox.app')
-    os.sys('open -n /Applications/Visual\ Studio\ Code.app')
-    os.sys('open -n /Applications/Splashtop\ Business.app')
+    os.system('open -n /Applications/Google Chrome.app')
+    os.system('open -n /Applications/1Password.app')
+    os.system('open -n /Applications/Firefox.app')
+    os.system('open -n /Applications/Visual Studio Code.app')
+    os.system('open -n /Applications/Splashtop Business.app')
     
     # Launch brew services
-    os.sys('brew services start colima')
-    os.sys('brew services start ollama')
+    os.system('brew services start colima')
+    os.system('brew services start ollama')
 
 def cleanup_and_exit():
     # setup sbemode folder
-    os.sys('mkdir ~/sbemode')
-    os.sys('mkdir ~/sbemode/code')
+    os.system('mkdir ~/sbemode')
+    os.system('mkdir ~/sbemode/code')
     print('DO NOT FORGET to do the following things before you are done!')
-    print('----> run 'gh auth' to get github cli setup')
+    print('----> run "gh auth" to get github cli setup')
     print('----> Clone orgmode, ai_materials, and other code repos')
-    os.sys('cd ~/sbemode')
-    os.sys('figlet DONE')
+    os.system('cd ~/sbemode')
+    os.system('figlet DONE')
     sys.exit(1)
     
 def backup():
     # Implement the backup logic here
-    os.system("figlet BackItUp")
+    os.systemtem("figlet BackItUp")
     print("Backing up critical files")
     time.sleep(1)
     
@@ -102,39 +104,39 @@ def backup():
     
     # Backup Emacs Config
     print(">>> Backing up emacs config (~/.emacs)")
-    os.system("cp ~/.emacs ~/sbemode/buildbot/configs/backup.emacs.lsp")
+    os.systemtem("cp ~/.emacs ~/sbemode/buildbot/configs/backup.emacs.lsp")
     time.sleep(1)
     
     # Backup Zshrc Config
     print(">>> Backing up zsh config (~/.zshrc)")
-    os.system("cp ~/.zshrc ~/sbemode/buildbot/configs/backup.zshrc")
+    os.systemtem("cp ~/.zshrc ~/sbemode/buildbot/configs/backup.zshrc")
     time.sleep(1)
     
     # Backup OhMyPosh Config
     print(">>> Backing up oh-my-posh config (~/.mytheme.omp.json)")
-    os.system("cp ~/.mytheme.omp.json ~/sbemode/buildbot/configs/backup.mytheme.omp.json")
+    os.systemtem("cp ~/.mytheme.omp.json ~/sbemode/buildbot/configs/backup.mytheme.omp.json")
     time.sleep(1)
     
     # Clean out emacs backups
     print("A little housekeeping, now...")
     print(">>> Cleaning out temp files in ~/zzzemacs-backups")
-    os.system("rm ~/zzzemacs-backups/*")
+    os.systemtem("rm ~/zzzemacs-backups/*")
     time.sleep(1)
     
     # All Done...
-    os.system("figlet COMPLETE")
+    os.systemtem("figlet COMPLETE")
     print("All critical files have been backed up.")
     # get files into git(hub)
     commit_now = input("Should I push these changes to git(hub) for you now? (Y/N): ")
     if commit_now.upper() == "Y":
         timestamp = time.strftime("%Y-%m-%d @ %H:%M:%S")
-        os.system("git add .")
-        os.system(f'git commit -m "Backup of critical files {timestamp}"')
-        os.system("git push -u origin main")
-        os.system("figlet GIT-ED")    
+        os.systemtem("git add .")
+        os.systemtem(f'git commit -m "Backup of critical files {timestamp}"')
+        os.systemtem("git push -u origin main")
+        os.systemtem("figlet GIT-ED")    
     else:
         print("DO NOT FORGET TO PUSH LATER, FOOL!")
-        os.sys("figlet DONE")B 
+        os.system("figlet DONE")
     sys.exit(1)
 
 def main():
